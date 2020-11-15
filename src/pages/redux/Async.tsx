811c37e92f6420248b7b2fd09b48798e12423ecc
释 <hoppergoo@gmail.com>
2020-11-15 21:38:30 +0800

    redux-thunk 使用

--- src/pages/redux/Async.tsx
+++ src/pages/redux/Async.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, bindActionCreators } from "redux";
 import { Provider, connect } from "react-redux";
 import thunk from "redux-thunk";
 import { fetchListReducer } from "../../redux/reducers/fetchList";
 import { fetchList } from "../../redux/actions/fetchList";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 function RenderList({ result }: any) {
   if (!result) return <span>初始状态</span>;
@@ -17,17 +18,20 @@ function RenderList({ result }: any) {
   );
 }
 
-const store = createStore(fetchListReducer, applyMiddleware(thunk));
+const store = createStore(
+  fetchListReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 const mapStateToProp = (state: any) => ({ result: state });
-const doFetchList = fetchList();
+const doFetchList = bindActionCreators(fetchList, store.dispatch);
 
 const RenderListContainer = connect(mapStateToProp)(RenderList);
 
 export default function ReduxAsync() {
   return (
     <Provider store={store}>
-      <button onClick={() => doFetchList(store.dispatch)}>获取数据</button>
+      <button onClick={doFetchList}>获取数据</button>
       <RenderListContainer />
     </Provider>
   );
